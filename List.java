88cb9777ab664606cfe63151d8cec9af043bd915
y0111930 <e.guenes@tu-braunschweig.de>
2023-06-04 12:13:46 +0200

    List.java

--- List.java
+++ List.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+//HasanCets tesekkurler
+public class List {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutu: ");
+        int n = inp.nextInt();
+
+        double[] arr = new double[n];
+        System.out.print("Dizi elemanlarını giriniz :");
+        for (int i = 0; i < n; i++){
+            arr[i] = inp.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += 1.0 / arr[i];
+        }
+
+        double harmonicMean = n / sum;
+        System.out.println("Dizinin harmonik ortalaması :" + harmonicMean);
+    }
+}
+/*
 public class List{
 	public static void main(String[] args) {
 		int[] list = {15, 4, 6, 45, 5, 23};
@@ -10,3 +35,4 @@ public class List{
 		// es gibt den Durchschnitt der Zahlen aus.
 	}
 }
+*/
