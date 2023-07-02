4f31be6faa436e902f09df93bb0f7d65365d39f7
y0111930 <e.guenes@tu-braunschweig.de>
2023-07-02 23:45:23 +0200

    efsan

--- Hausaufgaben2022WiSe/pflichtaufgabe2/MasterMind.java
+++ Hausaufgaben2022WiSe/pflichtaufgabe2/MasterMind.java
@@ -1,88 +1,84 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class MasterMind {
-
-    private static Random rnd = new Random();
     private static Scanner scan = new Scanner(System.in);
-    private static int[] code = new int[4];
-    
-    //int a = rnd.nextInt(8) + 1 --> eine zufallige zahl von 1 bis 8;
+    private static Random rnd = new Random();
+    private static int versuche = 0;
+    private static int code[];
     public static void main(String[] args) {
-        String codeString = codeErsteller();
-        int versuche = 0;
-        System.out.println("\nWilkommen! In diesem Spiel ist Ihre Aufgabe : dieses Code zu cracken.\n" +
-                            "Das code besteht aus 4 Ziffern, die unterschiedlich sind!\n" + 
-                            "Außerdem sind die Ziffern von 1 bis 8\n");
-        while (versuche != 10) {
-            System.out.print("Geben Sie Ihre Annahme ein : ");
-            String eingabe = scan.nextLine();
-            if (eingabeControl(eingabe)) {
-                if (codeRight( eingabe , codeString )) {
-                    System.out.println("Sie hats gefunden : Code : " + codeString + "\tEingabe : " + eingabe);
-                    break;
-                } else {
-                    codeControl( eingabe , codeString );
-                    versuche++;
+        System.out.println("\nWilkommen zu Mastermind\n" + "Sie sollen das von Ihnen entschiedene stelliges Code cracken!\n" + "Die Ziffern" + 
+                        " sind untrerschiedlich und Sie bekommen ein Feedback, nachdem Sie eine Zahl eingegeben haben!\n");
+        System.out.print("Geben Sie die Stelle(min 4, max 8) : ");
+        int x = scan.nextInt();
+        initCode(x);
+        String codeSon = codeErsteller();
+        while (versuche < 10) {
+            String a = scan.nextLine();
+            if (a.length()==code.length) {
+                if (Integer.parseInt(a) > 0) {
+                    if (a.equals(codeSon)) {
+                        System.out.println("Sie haben das Code gecrackt!!\nIhre Eingabe : " + a + " | das Code : " + codeSon);
+                        break;
+                    } else {
+                        codeControl(a , codeSon);
+                        System.out.println();
+                        versuche++;
+                    }
                 }
             } else {
-                System.out.println("Eingabe muss 4 stellig sein!\n");
-                continue;
+                System.out.println("Die versuchende Zahl muss " + code.length + " stellig sein!\n");
             }
+            
         }
-        if (versuche==10) {
-            System.out.println(codeString);
+        if (versuche == 10) {
+            System.out.println("Ihre Versuchanzahl : " + versuche);
+            System.out.println("Sie haben verloren! Das code war : " + codeSon);
         }
-        
-
     }
 
-    public static void codeControl(String a , String b) {
-        if ( eingabeControl(a) ) {
-            for (int i=0; i<4; i++) {
-                for (int j=0; j<4; j++) {
-                    if ( a.charAt(i) == b.charAt(j)) {
-                        if (i == j) {
-                            System.out.println(Character.toString(a.charAt(i)) + " ist ricthig");
-                        } else {
-                            System.out.println(Character.toString(b.charAt(j)) + " ist enthalten!");
-                        }
+    public static void initCode(int a) {
+        code = new int[a];
+    }
+
+    public static void codeControl(String b , String c) {
+        assert b.length()==code.length && c.length()==code.length;
+        for (int i=0; i<code.length; i++) {
+            for (int j=0; j<code.length; j++) {
+                if (b.charAt(i) == c.charAt(j)) {
+                    if (i==j) {
+                        System.out.println(b.charAt(i) + " ist richtig und in der richtigen Stelle!");
+                    } else {
+                        System.out.println(b.charAt(i) + " ist richtig, aber in der falschen Stelle!");
                     }
                 }
             }
-        }
-    }
 
-    public static boolean codeRight( String a , String b ) {
-        if ( eingabeControl(a) && eingabeControl(b) ) {
-            if ( a.equals(b) ) {
-                return true;
-            }
         }
-        return false;
     }
 
     public static String codeErsteller() {
-        for (int i=0; i<4; i++) {
+        String s = "";
+        for (int i=0; i<code.length; i++) {
             code[i] = rnd.nextInt(8) + 1;
+            s += String.valueOf(code[i]);
         }
-
-        if ((code[0] != code[1]) && (code[1] != code[2]) && (code[2] != code[3])) {
-            String codeSon = "";
-            for (int i=0; i<4; i++) {
-                codeSon += String.valueOf(code[i]);
-            }
-            return codeSon;
+        if (check(code)) {
+            return s;
         } else {
             return codeErsteller();
         }
     }
 
-    public static boolean eingabeControl(String s) {
-        if (s.length() == 4) {
-            return true;
+    public static boolean check(int[] arr) {
+        assert arr.length == code.length;
+        for (int i=0; i<code.length-1; i++) {
+            for (int j = i+1; j<code.length; j++) {
+                if ( arr[i] == arr[j] ) {
+                    return false;
+                }
+            }
         }
-        return false;
+        return true;
     }
-    
 }
